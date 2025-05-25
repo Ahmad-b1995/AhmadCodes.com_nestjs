6d45c388b84b0a970faf0266913a23acb2e0ce7e
Ahmad <ahmad.b1995@gmail.com>
2025-05-25 21:45:35 +0330

    swagger and bug fix

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('APP_PORT') || 3100;
+
+  const config = new DocumentBuilder()
+    .setTitle('AhmadCodes.com')
+    .setDescription('AhmadCodes.com API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 }
 bootstrap();
